42da70497df5ff9c167be1260edbb200af07b31c
aa-ho <aaron.hoeper1@gmx.de>
2023-02-26 03:08:07 +0100

    fixes

--- src/main/java/cores/api/Scoreboard.kt
+++ src/main/java/cores/api/Scoreboard.kt
@@ -24,24 +24,22 @@ class Scoreboard {
         objective.getScore("").score = 12
         objective.getScore("Du spielst§8:").score = 11
         objective.getScore(PREFIX_COLORED).score = 10
-
+        objective.getScore(" ").score = 9
         if (plugin.gameStateManager.lobbyState.lobbyCountdown.isIdling) {
             objective.getScore("§fStart in§8:").score = 8
-            objective.getScore(scoreboardLobbyCountdown(plugin.gameStateManager.lobbyState.lobbyCountdown.seconds)).score =
-                7
+            objective.getScore(scoreboardLobbyCountdown(plugin.gameStateManager.lobbyState.lobbyCountdown.seconds)).score = 7
         } else {
             objective.getScore("§fSpieler§8:").score = 8
             objective.getScore(getPlayersScoreboard()).score = 7
         }
 
-        objective.getScore(" ").score = 6
+        objective.getScore("  ").score = 6
         objective.getScore("§fMap§8:").score = 5
         objective.getScore("§eMünchen").score = 4
-        objective.getScore("  ").score = 3
+        objective.getScore("   ").score = 3
         objective.getScore("§fTeam§8:").score = 2
-        objective.getScore(if (plugin.teamHelper.isPlayerInTeam(p)) if (plugin.teamHelper.getPlayerTeam(p) == Teams.RED) RED_COLORED else BLUE_COLORED else Messages.RANDOM_TEAM_COLORED).score =
-            1
-        objective.getScore("   ").score = 0
+        objective.getScore(if (plugin.teamHelper.isPlayerInTeam(p)) if (plugin.teamHelper.getPlayerTeam(p) == Teams.RED) RED_COLORED else BLUE_COLORED else Messages.RANDOM_TEAM_COLORED).score = 1
+        objective.getScore("    ").score = 0
 
         p.scoreboard = scoreboard
     }
